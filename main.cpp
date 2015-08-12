fc31e56a1a316ad507196745871f63c4c5bf9469
stavinsky <stavinsky@gmail.com>
2015-08-13 01:43:37 +0300

    phreads

--- main.cpp
+++ main.cpp
@@ -2,14 +2,14 @@
 #include <windows.h>
 #include <stdio.h>
 #include "client.h"
-
+#include <thread>
 
 
  
 int main(int argc, char **argv)
 {
     Client client("127.0.0.1", "9999");
-    client.loop();
+    std::thread t(client.loop());
     std::cout << "test" << std::endl;
 
     return 0;
